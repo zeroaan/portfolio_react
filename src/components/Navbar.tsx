4ab63e6ac57cfb647e4948059791c3d1384adeed
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-17 17:49:59 +0900

    Add: navbar logo, git

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,26 +3,47 @@ import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+import LOGO from "assets/images/logo.png";
+import GITHUB from "assets/images/github.png";
+
 const DivStyled = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   color: white;
   background-color: black;
   height: 60px;
+  user-select: none;
 `;
 const LinkStyled = styled(Link)<{ $current: boolean }>`
-  padding: 0 30px;
+  padding: 12px 30px;
   font-size: 18px;
   text-align: center;
-  height: 100%;
-  line-height: 60px;
   opacity: ${(props) => (props.$current ? 1 : 0.6)};
   border-bottom: 5px solid ${(props) => (props.$current ? "rgb(60, 173, 212)" : "transparent")};
   transition: all 0.3s linear;
   &:hover {
     opacity: 1;
   }
+`;
+const ImgLogo = styled.img`
+  height: 40px;
+  margin: 0 30px;
+  pointer-events: none;
+`;
+const AGithub = styled.a`
+  margin: 0 30px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50px;
+  transition: all 0.3s ease;
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+const ImgGithub = styled.img`
+  height: 40px;
+  pointer-events: none;
 `;
 
 const Navbar = () => {
@@ -31,15 +52,21 @@ const Navbar = () => {
   return (
     <>
       <DivStyled>
-        <LinkStyled to="/" $current={pathname === "/"}>
-          Home
-        </LinkStyled>
-        <LinkStyled to="/about" $current={pathname === "/about"}>
-          About
-        </LinkStyled>
-        <LinkStyled to="/project" $current={pathname === "/project"}>
-          Project
-        </LinkStyled>
+        <ImgLogo src={LOGO} alt="LOGO" />
+        <div>
+          <LinkStyled to="/" $current={pathname === "/"}>
+            Home
+          </LinkStyled>
+          <LinkStyled to="/about" $current={pathname === "/about"}>
+            About
+          </LinkStyled>
+          <LinkStyled to="/project" $current={pathname === "/project"}>
+            Project
+          </LinkStyled>
+        </div>
+        <AGithub href="https://github.com/zeroaan" target="_blank">
+          <ImgGithub src={GITHUB} alt="GITHUB" />
+        </AGithub>
       </DivStyled>
     </>
   );
