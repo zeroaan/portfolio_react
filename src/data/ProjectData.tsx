1de558a1deeceb6f6d49d61224749d9b8fd145bc
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-18 17:00:48 +0900

    Add: project

---
+++ src/data/ProjectData.tsx
@@ -0,0 +1,59 @@
+import PROJECT01 from "assets/images/project/project01.png";
+import PROJECT02 from "assets/images/project/project02.png";
+import PROJECT03 from "assets/images/project/project03.png";
+import PROJECT04 from "assets/images/project/project04.png";
+import PROJECT05 from "assets/images/project/project05.png";
+import PROJECT06 from "assets/images/project/project06.png";
+import PROJECT07 from "assets/images/project/project07.png";
+
+export const ProjectData = [
+  {
+    id: 7,
+    title: "Team Con Project",
+    subTitle: "Kosta 팀 프로젝트",
+    github: "https://github.com/zeroaan/con_web",
+    src: PROJECT07,
+  },
+  {
+    id: 6,
+    title: "Trello",
+    subTitle: "Typescript, Redux, CSS in JS, Drag & Drop",
+    github: "https://github.com/zeroaan/trello",
+    src: PROJECT06,
+  },
+  {
+    id: 5,
+    title: "To Do List",
+    subTitle: "Redux를 사용한 상태관리",
+    github: "https://github.com/zeroaan/todolist",
+    src: PROJECT05,
+  },
+  {
+    id: 4,
+    title: "Every Moment",
+    subTitle: "React, Firebase를 이용한 실시간 커뮤니티",
+    github: "https://github.com/zeroaan/everymoment",
+    src: PROJECT04,
+  },
+  {
+    id: 3,
+    title: "Movie Game",
+    subTitle: "영화 포스터를 보고 개봉년도 맞추기",
+    github: "https://github.com/zeroaan/movie_game",
+    src: PROJECT03,
+  },
+  {
+    id: 2,
+    title: "Movie App",
+    subTitle: "React를 이용해 만든 첫 사이트",
+    github: "https://github.com/zeroaan/what_to_watch",
+    src: PROJECT02,
+  },
+  {
+    id: 1,
+    title: "Portfolio Site",
+    subTitle: "HTML, CSS, JS 만으로만 만든 사이트",
+    github: "https://github.com/zeroaan/portfolio",
+    src: PROJECT01,
+  },
+];
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,36 +1,89 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import Layout from "components/Layout";
 
+import { ProjectData } from "data/ProjectData";
+
+const fadeIn = keyframes` 
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const PGithub = styled.p`
+  font-size: 20px;
+  color: white;
+  padding: 100px 0 5px 0;
+  text-align: center;
+  user-select: none;
+`;
 const DivContainer = styled.div`
-  padding-top: 100px;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   flex-wrap: wrap;
   user-select: none;
   margin: auto;
-  width: 1000px;
+  width: 920px;
+  animation: ${fadeIn} 0.5s linear;
 `;
-const DivProjects = styled.div`
+const DivProImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 30px;
+  transition: all 0.5s ease-in-out;
+`;
+const AProjects = styled.a`
+  position: relative;
   margin: 35px;
   width: 390px;
   height: 230px;
-  border-radius: 20px;
-  background-color: rgb(246, 248, 254);
+  border-radius: 30px;
+  &:hover ${DivProImg} {
+    transform: scale(0.5) translateY(-100px);
+  }
+`;
+const DivProBack = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  border-radius: 30px;
+  background-color: rgb(20, 20, 20);
+  z-index: -1;
+  color: white;
+`;
+const PTitle = styled.p`
+  font-size: 24px;
+`;
+const PSubTitle = styled.p`
+  font-size: 14px;
+  margin: 18px 0 28px 0;
 `;
 
 const Project = () => {
   return (
     <Layout>
+      <PGithub>Click to go to the GitHub</PGithub>
       <DivContainer>
-        <DivProjects></DivProjects>
-        <DivProjects></DivProjects>
-        <DivProjects></DivProjects>
-        <DivProjects></DivProjects>
-        <DivProjects></DivProjects>
-        <DivProjects></DivProjects>
+        {ProjectData.map((v) => (
+          <AProjects key={v.id} href={v.github} target="_blank">
+            <DivProImg src={v.src} alt={v.title} />
+            <DivProBack>
+              <PTitle>{v.title}</PTitle>
+              <PSubTitle>{v.subTitle}</PSubTitle>
+            </DivProBack>
+          </AProjects>
+        ))}
       </DivContainer>
     </Layout>
   );
