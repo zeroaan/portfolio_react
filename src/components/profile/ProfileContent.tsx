f9d26354b001dbac815927f7af77a616c2127204
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-12-21 17:23:32 +0900

    Update: profile margin

--- src/components/profile/ProfileContent.tsx
+++ src/components/profile/ProfileContent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const DivData = styled.div`
-  margin: 25px 0;
+  margin: 30px 0;
 `;
 const DivTitle = styled.div`
   display: flex;
